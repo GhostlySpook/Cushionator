38b2902df5c717c862c08a9b848bf4e964956eab
GhostlySpook <kitties.everywhere@hotmail.com>
2024-01-09 01:20:04 -0700

    Added insert, uppage, downpage

--- Cushionator/Form1.cs
+++ Cushionator/Form1.cs
@@ -31,6 +31,9 @@ namespace Cushionator
         private const string SOUND_PATH_BABY_GASP = @".\Resources\baby_gasp.wav";
         private const string SOUND_PATH_CAMERA = @".\Resources\camera.wav";
         private const string SOUND_PATH_WHONK = @".\Resources\whonk.wav";
+        private const string SOUND_PATH_MEOW = @".\Resources\meow.wav";
+        private const string SOUND_PATH_BARK = @".\Resources\bark.wav";
+        private const string SOUND_PATH_GNOME = @".\Resources\gnome.wav";
 
         private const string SOUND_PATH_WHOOPIE = @".\Resources\whoopie.wav";
         private const string SOUND_PATH_WHOOPIE_MINUS_4 = @".\Resources\whoopie-4.wav";
@@ -56,6 +59,9 @@ namespace Cushionator
         System.Media.SoundPlayer player_baby_gasp;
         System.Media.SoundPlayer player_camera;
         System.Media.SoundPlayer player_whonk;
+        System.Media.SoundPlayer player_meow;
+        System.Media.SoundPlayer player_bark;
+        System.Media.SoundPlayer player_gnome;
 
         System.Media.SoundPlayer player_whoopie;
         System.Media.SoundPlayer player_whoopie_minus_4;
@@ -99,8 +105,8 @@ namespace Cushionator
             /*8 col*/ new Keys[] { Keys.D8, Keys.I, Keys.K, Keys.Oemcomma/*, Keys.RMenu*/ },
             /*9 col*/ new Keys[] { Keys.D9, Keys.O, Keys.L, Keys.OemPeriod },
             /*0 col*/ new Keys[] { Keys.D0, Keys.P/*, Keys.OemMinus*/ },
-            /*' col*/ /*new Keys[] { Keys.Oemtilde, Keys.OemOpenBrackets },*/
-            /*¿ col*/ /*new Keys[] { Keys.OemQuestion, Keys.Oemplus, Keys.OemCloseBrackets, Keys.Menu },*/
+            /*' col*/ new Keys[] { /*Keys.Oemtilde,*/Keys.Insert, Keys.PageUp, Keys.PageDown },
+            /*¿ col*/ new Keys[] { /*Keys.OemQuestion*//*, Keys.Oemplus, Keys.OemCloseBrackets, Keys.Menu*/ },
             /*Backspace col*/ new Keys[] { Keys.Back, Keys.Enter, Keys.PrintScreen/*, Keys.RShiftKey, Keys.RControlKey*/ },
             /*Space col*/ new Keys[] { Keys.Space, Keys.OemMinus, Keys.Oemplus/*, Keys.RShiftKey, Keys.RControlKey*/ }
         };
@@ -120,6 +126,9 @@ namespace Cushionator
             player_baby_gasp = new System.Media.SoundPlayer(soundLocation: SOUND_PATH_BABY_GASP);
             player_camera = new System.Media.SoundPlayer(soundLocation: SOUND_PATH_CAMERA);
             player_whonk = new System.Media.SoundPlayer(soundLocation: SOUND_PATH_WHONK);
+            player_meow = new System.Media.SoundPlayer(soundLocation: SOUND_PATH_MEOW);
+            player_bark = new System.Media.SoundPlayer(soundLocation: SOUND_PATH_BARK);
+            player_gnome = new System.Media.SoundPlayer(soundLocation: SOUND_PATH_GNOME);
 
             //1 - Define whoopie sounds
             player_whoopie = new System.Media.SoundPlayer(soundLocation: SOUND_PATH_WHOOPIE);
@@ -271,6 +280,15 @@ namespace Cushionator
                     case Keys.PrintScreen:
                         player_camera.Play();
                         break;
+                    case Keys.Insert:
+                        player_gnome.Play();
+                        break;
+                    case Keys.PageUp:
+                        player_meow.Play();
+                        break;
+                    case Keys.PageDown:
+                        player_bark.Play();
+                        break;
                     }
 
                 //Make sound depending of number
@@ -295,7 +313,7 @@ namespace Cushionator
                 else
                 {
                     KeyHandler found;
-                    Keys[] otherKeys = { Keys.PrintScreen, Keys.Oemplus, Keys.OemMinus, Keys.Tab, Keys.Space, Keys.Enter, Keys.Back, Keys.Oemcomma, Keys.OemPeriod, Keys.Escape };
+                    Keys[] otherKeys = { Keys.Insert, Keys.PageUp, Keys.PageDown, Keys.PrintScreen, Keys.Oemplus, Keys.OemMinus, Keys.Tab, Keys.Space, Keys.Enter, Keys.Back, Keys.Oemcomma, Keys.OemPeriod, Keys.Escape };
 
                     if(Array.Find(otherKeys, x => x == keyId) != 0)
                     {
@@ -343,6 +361,18 @@ namespace Cushionator
 
                             case Keys.PrintScreen:
                                 keyToSend = "{PRTSC}";
+                                break;
+
+                            case Keys.Insert:
+                                keyToSend = "{INSERT}";
+                                break;
+
+                            case Keys.PageUp:
+                                keyToSend = "{PGUP}";
+                                break;
+
+                            case Keys.PageDown:
+                                keyToSend = "{PGDN}";
                                 break;
                         }
 
