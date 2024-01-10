289987545cfb2668da11a94167576c90a8055a81
GhostlySpook <kitties.everywhere@hotmail.com>
2024-01-09 19:07:51 -0700

    Added signature

--- Cushionator/Form1.Designer.cs
+++ Cushionator/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.revealToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.selfDestructToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -45,7 +46,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(985, 428);
+            this.button1.Location = new System.Drawing.Point(630, 398);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(48, 41);
             this.button1.TabIndex = 0;
@@ -117,11 +118,23 @@
             this.selfDestructToolStripMenuItem.Text = "Self-Destruct";
             this.selfDestructToolStripMenuItem.Click += new System.EventHandler(this.selfDestructToolStripMenuItem_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label3.Enabled = false;
+            this.label3.Location = new System.Drawing.Point(964, 462);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ghostlyspook";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1045, 484);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
@@ -149,6 +162,7 @@
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem revealToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem selfDestructToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
     }
 }
 
